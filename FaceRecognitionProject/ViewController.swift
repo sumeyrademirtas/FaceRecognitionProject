580edc3abc122cc00b65dbe5c9c0c948738090de
sumeyrademirtas <smyrdmrts@gmail.com>
2024-06-11 11:46:23 +0300

    feat: add biometric authentication with LAContext
    - implemented signInClicked action to handle biometric authentication using LAContext.
    - added logic to check if biometric authentication is available and can be evaluated.
    - utilized evaluatePolicy to perform biometric authentication with a localized reason.
    - displayed a success message and performed segue on successful authentication.
    - updated myLabel to show an error message on authentication failure.

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext() // Dogrulama islemleri icin kullanilan bir obje
+        
+        var error : NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                
+                DispatchQueue.main.async {
+                    if success == true {
+                        //successfull aut
+                        self.performSegue(withIdentifier: "toSecond", sender: nil)
+                    } else {
+                        self.myLabel.text = "Error!"
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
 }
-
