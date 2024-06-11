28adc9c2771d62dd12844da393fe7c4eb973615d
sumeyrademirtas <smyrdmrts@gmail.com>
2024-06-11 10:56:50 +0300

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Sümeyra Demirtaş on 11.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
